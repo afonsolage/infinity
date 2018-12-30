b617dbdca0a6712472b986e1e9b0ff23a0ae1122
Afonso Lage <afonsolage@sciosolutions.com.br>
2018-12-30 14:48:34 -0300

    Added animator class

---
+++ core/src/com/lagecompany/infinity/anim/Animator.java
@@ -0,0 +1,95 @@
+package com.lagecompany.infinity.anim;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Json;
+import com.lagecompany.infinity.anim.AnimationData.AnimationDataRegion;
+
+public class Animator extends Actor {
+	private static final String LOG_TAG = Animator.class.getSimpleName();
+
+	private final Map<String, Animation<TextureRegion>> animationMap;
+	private String currentAnimationName;
+	private Animation<TextureRegion> currentAnimation;
+	private float stateTime;
+
+	public Animator() {
+		this.animationMap = new HashMap<>();
+	}
+
+	public String addAnimation(String path) {
+		try {
+			FileHandle fileHandle = Gdx.files.internal(path);
+			Json json = new Json();
+			AnimationData data = json.fromJson(AnimationData.class, fileHandle);
+
+			if (data == null) {
+				Gdx.app.error(LOG_TAG, "Failed to parse animation file " + path);
+			} else {
+				Texture texture = new Texture(Gdx.files.internal(data.getTexturePath()));
+
+				List<TextureRegion> regions = new ArrayList<>();
+				for (AnimationDataRegion dataRegion : data.getRegions()) {
+					regions.add(new TextureRegion(texture, dataRegion.getX(), dataRegion.getY(), dataRegion.getWidth(),
+							dataRegion.getHeight()));
+				}
+
+				Animation<TextureRegion> animation = new Animation<TextureRegion>(data.getFrameTime(),
+						regions.toArray(new TextureRegion[regions.size()]));
+				animation.setPlayMode(PlayMode.LOOP);
+				animationMap.put(data.getName(), animation);
+				return data.getName();
+			}
+		} catch (Exception e) {
+			Gdx.app.error(LOG_TAG, "Failed to load animation at path " + path, e);
+		}
+		return null;
+	}
+
+	public boolean playAnimation(String name) {
+		Animation<TextureRegion> animation = animationMap.get(name);
+
+		if (animation == null)
+			return false;
+
+		currentAnimationName = name;
+		currentAnimation = animation;
+		stateTime = 0f;
+		return true;
+	}
+
+	public String getCurrentAnimationName() {
+		return currentAnimationName;
+	}
+
+	public void stopAnimation() {
+		currentAnimation = null;
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		if (currentAnimation == null)
+			return;
+
+		TextureRegion region = currentAnimation.getKeyFrame(stateTime);
+		batch.draw(region, getX(), getY());
+	}
+
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+		stateTime += delta;
+	}
+
+}
--- core/src/com/lagecompany/infinity/object/Player.java
+++ core/src/com/lagecompany/infinity/object/Player.java
@@ -1,46 +1,19 @@
 package com.lagecompany.infinity.object;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.lagecompany.infinity.anim.Animator;
 
-public class Player extends Actor {
+public class Player extends Group {
 
-	private static final int FRAMES = 6;
-	private static final float FRAME_TIME = 0.1f;
-	private static final float ANIM_TIME = FRAMES * FRAME_TIME;
+	private Animator animator;
 
-	private Animation<TextureRegion> walkAnimation;
-	private Texture walkSheet;
-
-	float stateTime;
-	float limit;
-
-	//TODO: Develop sprite sheet map or something like that and use ragna sprite sheet for dev
-	
 	public Player() {
-		walkSheet = new Texture(Gdx.files.internal("W_E.png"));
+		animator = new Animator();
+		animator.addAnimation("anim/player/walk_sw.anim");
+		animator.playAnimation("walk_sw");
 
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / FRAMES, walkSheet.getHeight());
-		TextureRegion[] walkFrames = tmp[0];
-
-		walkAnimation = new Animation<>(FRAME_TIME, walkFrames);
-		stateTime = 0;
-	}
-
-	@Override
-	public void draw(Batch batch, float parentAlpha) {
-		TextureRegion region = walkAnimation.getKeyFrame(stateTime);
-		batch.draw(region, 50, 50);
-	}
-
-	@Override
-	public void act(float delta) {
-		stateTime += delta;
-		if (stateTime > ANIM_TIME)
-			stateTime = 0;
+		animator.setPosition(50, 50);
+		
+		addActor(animator);
 	}
 }
