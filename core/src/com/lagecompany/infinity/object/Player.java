29f93a92270a7898cc61373fbf7d392375c46c62
Afonso Lage <lage.afonso@gmail.com>
2018-12-30 22:25:50 -0300

    Finished basic player moviment

--- core/src/com/lagecompany/infinity/object/Player.java
+++ core/src/com/lagecompany/infinity/object/Player.java
@@ -8,7 +8,7 @@ import com.lagecompany.infinity.input.InputObserver;
 
 public class Player extends Group {
 
-	private static final String path = "player/";
+	private static final String ANIM_PATH = "player/";
 	private static final String WALK_S = "walk_s";
 	private static final String WALK_SW = "walk_sw";
 	private static final String WALK_W = "walk_w";
@@ -18,22 +18,20 @@ public class Player extends Group {
 	private static final String WALK_E = "walk_e";
 	private static final String WALK_N = "walk_n";
 
+	private static final String[] ANIMATIONS = { WALK_S, WALK_SW, WALK_W, WALK_NW, WALK_NE, WALK_SE, WALK_E, WALK_N };
+
 	private Animator animator;
 
 	private Vector2 dir;
+	private float moveSpeed = 60f;
 
 	public Player() {
 		dir = new Vector2();
 
 		animator = new Animator();
-		animator.addAnimation("player/walk_s");
-		animator.addAnimation("player/walk_sw");
-		animator.addAnimation("player/walk_se");
-		animator.addAnimation("player/walk_n");
-		animator.addAnimation("player/walk_nw");
-		animator.addAnimation("player/walk_ne");
-		animator.addAnimation("player/walk_w");
-		animator.addAnimation("player/walk_e");
+		for (String anim : ANIMATIONS) {
+			animator.addAnimation(ANIM_PATH + anim);
+		}
 
 		InputObserver.onKeyDown(this::onKeyDown);
 		InputObserver.onKeyUp(this::onKeyUp);
@@ -88,24 +86,43 @@ public class Player extends Group {
 	public void act(float delta) {
 		super.act(delta);
 
+		Vector2 tmp = new Vector2(getX(), getY());
+
 		if (dir.x == 0 && dir.y == 0 && animator.isPlaying()) {
 			animator.stopAnimation();
 		} else if (dir.x > 0 && dir.y == 0) {
 			animator.playAnimation(WALK_E);
-		} else if (dir.x > 0 && dir.y > 0) {
-			animator.playAnimation(WALK_NE);
+		} else if (dir.x < 0 && dir.y == 0) {
+			animator.playAnimation(WALK_W);
+		} else if (dir.x == 0 && dir.y < 0) {
+			animator.playAnimation(WALK_S);
 		} else if (dir.x == 0 && dir.y > 0) {
 			animator.playAnimation(WALK_N);
 		} else if (dir.x < 0 && dir.y > 0) {
 			animator.playAnimation(WALK_NW);
-		} else if (dir.x < 0 && dir.y == 0) {
-			animator.playAnimation(WALK_W);
+		} else if (dir.x > 0 && dir.y > 0) {
+			animator.playAnimation(WALK_NE);
 		} else if (dir.x < 0 && dir.y < 0) {
 			animator.playAnimation(WALK_SW);
-		} else if (dir.x == 0 && dir.y < 0) {
-			animator.playAnimation(WALK_S);
 		} else if (dir.x > 0 && dir.y < 0) {
 			animator.playAnimation(WALK_SE);
 		}
+
+		if (dir.x == 0 && dir.y == 0)
+			return;
+
+		Vector2 norDir = new Vector2(dir);
+		norDir.nor();
+
+		tmp.x += norDir.x * moveSpeed * delta;
+		tmp.y += norDir.y * moveSpeed * 0.5f * delta;
+
+		setPosition(tmp.x, tmp.y);
 	}
+
+	@Override
+	protected void positionChanged() {
+		animator.setPosition(getX(), getY());
+	}
+
 }
