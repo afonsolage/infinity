8d052098a825fe746d8a374fe756f9a6082602cf
Afonso Lage <afonsolage@sciosolutions.com.br>
2018-12-31 22:32:06 -0300

    Switched to IntelliJ due to better Kotlin support

--- core/src/main/kotlin/com/lagecompany/infinty/animation/Animator.kt
+++ core/src/main/kotlin/com/lagecompany/infinty/animation/Animator.kt
@@ -10,57 +10,63 @@ import com.badlogic.gdx.utils.Json
 
 data class AnimationRegion(val x: Int, val y: Int, val width: Int, val height: Int)
 data class AnimationData(
-	val name: String,
-	val texturePath: String,
-	val frameTime: Float,
-	val flip: Boolean,
-	val loop: Boolean
+        val name: String,
+        val texturePath: String,
+        val frameTime: Float,
+        val flip: Boolean,
+        val loop: Boolean
 ) {
-	val regions = mutableListOf<AnimationRegion>()
+    val regions = mutableListOf<AnimationRegion>()
 }
 
 private const val LOG_TAG = "ANIMATOR"
 
 class Animator : Actor() {
-	private val animationMap = mutableMapOf<String, Animation<TextureRegion>>()
+    private val animationMap = mutableMapOf<String, Animation<TextureRegion>>()
 
-	private lateinit var currentAnimation: Animation<TextureRegion>
-	private var curAnimName = ""
-	private var stateTime = 0f
+    private lateinit var curAnim: Animation<TextureRegion>
+    private var curAnimName = ""
+    private var stateTime = 0f
 
-	fun addAnimation(path: String) {
-		try {
-			val fileHandle = Gdx.files.internal("anim/$path.anim")
-			val data = Json().fromJson(AnimationData::class.java, fileHandle)
+    fun addAnimation(path: String) {
+        try {
+            val fileHandle = Gdx.files.internal("anim/$path.anim")
+            val data = Json().fromJson(AnimationData::class.java, fileHandle)
 
-			if (data != null) {
-				val texture = Texture(Gdx.files.internal(data.texturePath))
+            if (data != null) {
+                val texture = Texture(Gdx.files.internal(data.texturePath))
 
-				var regions = mutableListOf<TextureRegion>()
-				for (dataRegion in data.regions) {
-					val region = TextureRegion(texture, dataRegion.x, dataRegion.y, dataRegion.width, dataRegion.height)
-					if (data.flip)
-						region.flip(true, false)
-					regions.add(region)
-				}
+                var regions = mutableListOf<TextureRegion>()
+                for (dataRegion in data.regions) {
+                    val region = TextureRegion(texture, dataRegion.x, dataRegion.y, dataRegion.width, dataRegion.height)
+                    if (data.flip)
+                        region.flip(true, false)
+                    regions.add(region)
+                }
 
-				var animation = Animation<TextureRegion>(data.frameTime, *regions.toTypedArray())
-				animation.setPlayMode(if (data.loop) PlayMode.LOOP else PlayMode.NORMAL)
-				animationMap.put(data.name, animation)
-			} else {
-				Gdx.app.error(LOG_TAG, "Failed to parse animation file $path")
-			}
-		} catch (e: Exception) {
-			Gdx.app.error(LOG_TAG, "Failed to load animation at path $path", e)
-		}
-	}
+                var animation = Animation<TextureRegion>(data.frameTime, *regions.toTypedArray())
+                animation.setPlayMode(if (data.loop) PlayMode.LOOP else PlayMode.NORMAL)
+                animationMap.put(data.name, animation)
+            } else {
+                Gdx.app.error(LOG_TAG, "Failed to parse animation file $path")
+            }
+        } catch (e: Exception) {
+            Gdx.app.error(LOG_TAG, "Failed to load animation at path $path", e)
+        }
+    }
 
-	fun playAnimation(name: String): Boolean {
-		if (name.equals(name, ignoreCase = true))
-			return false
+    fun playAnimation(name: String): Boolean {
+        if (name.equals(name, ignoreCase = true))
+            return false
 
-		
+        val animation = animationMap.get(name) ?: return false
 
-		return true
-	}
+        curAnimName = name
+        curAnim = animation
+        stateTime = 0f
+
+        return true
+    }
+
+    
 }
